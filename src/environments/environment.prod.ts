56b725c88bd823a7adea09a8ee421c9fbd1b0938
Lucifer0885 <georgeskotidismain@gmail.com`>
2025-06-09 01:19:14 +0300

    feat(environment): add production environment configuration

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+const environment = {
+  production: true,
+  endpoint: 'https://api.efood.pagonoudis.gr/driver',
+  googleMapsApiKey: 'AIzaSyDDU8PFyo5R2GIJfRWKbrIiu6sDYVVCRTw',
+  socketUrl: 'https://sockets.efood.pagonoudis.gr',
+}
+
+export default environment;
